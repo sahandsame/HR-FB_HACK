4b1c39c0dd54725299b023e2d67df0cf2f4d8214
ReVa <58979786+sahandsame@users.noreply.github.com>
2020-06-29 20:07:15 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 echo update start soon
 cd
-rm -rif reva
-git clone https://github.com/sahandsame/reva
+rm -rif HR-FB_HACK
+git clone https://github.com/sahandsame/HR-FB_HACK
 echo update successfully 
 cd
-cd reva
+cd HR-FB_HACK
 python2 reva.py
